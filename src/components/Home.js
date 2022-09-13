52d824aaa853f0802af687191ab1ae931fdac9ea
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-09-13 12:38:27 +0300

    Yee

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,6 @@ import '../App.css';
 const Home = ({ user }) => {
   return (
     <div className="home">
-<<<<<<< HEAD
-=======
-      <h1 class="h1">Hello, <span></span>{user.displayName}</h1>
->>>>>>> dfd07ccf59e2e0f1adc217a1aa1151215d3eb533
       <img src={user.photoURL} alt="Avatar" />
       <button class="button-38" onClick={() => auth.signOut()}>Sign out</button>
       <h1>Hello, <span></span>{user.displayName}</h1>
