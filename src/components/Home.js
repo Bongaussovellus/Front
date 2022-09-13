4f1e86b26a28054fc142b150e909d54454dce151
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-09-13 12:28:14 +0300

    Css päivitys

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ import '../App.css';
 const Home = ({ user }) => {
   return (
     <div className="home">
-      <h1>Hello, <span></span>{user.displayName}</h1>
       <img src={user.photoURL} alt="Avatar" />
-      <button className="button signout" onClick={() => auth.signOut()}>Sign out</button>
+      <button class="button-38" onClick={() => auth.signOut()}>Sign out</button>
+      <h1>Hello, <span></span>{user.displayName}</h1>
     </div>
   )
 }
