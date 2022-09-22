8631a4f1e542cda1261aa931006ae03b98916488
Tuukka Nisonen <tuukkanis@gmail.com>
2022-09-22 12:11:07 +0300

    säätöä

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,32 @@
-const Map = () => {
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react';
+import '../App.css';
+
+
+class map extends Component{
+
+
+render(){ 
+  
     return (
       <div>
         <h1>Tänne kartta.</h1>
+        <Map class='map' google={this.props.google} zoom={10}>
+ 
+      
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
       </div>
     )
   }
+}
+
   
-  export default Map;
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyBZ8seLhFZ3P-J6hTW3lFyGGHKv-UpKD60")
+  })(Map);
