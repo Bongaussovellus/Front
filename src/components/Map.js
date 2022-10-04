318751498f624265be4e01d741510fb22719ff79
Tuukka Nisonen <tuukkanis@gmail.com>
2022-10-04 13:50:22 +0300

    Kartan ominaisuuksia lisätty

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,18 @@ import {GoogleMap, useLoadScript, InfoWindow, Marker, MarkerClusterer,} from '@r
 import React, { Component } from 'react';
 import { formatRelative } from "date-fns";
 import mapStyles from './mapStyles';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
 
 const GOOGLE_MAPS_API_KEY="AIzaSyBZ8seLhFZ3P-J6hTW3lFyGGHKv-UpKD60"
@@ -10,7 +22,6 @@ const GOOGLE_PLACES_API_KEY="AIzaSyD06HZ7zETSRxkfOLHxnapESbQqi9kKp78"
 const mapContainerStyle = {
   width: '100vw',
   height: '80vh',
-  
 };
 const center = {
   lat: 60.169857,
@@ -18,16 +29,16 @@ const center = {
 }
 
 const options = {
-  styles: mapStyles,
-  disableDefaultUI: true,
   zoomControl: true,
 };
 
+const libraries = ["places"];
 
 export default function Map() {
   const {isLoaded, loadError} = useLoadScript ({
     googleMapsApiKey: GOOGLE_MAPS_API_KEY,
-    googlePlacesApiKey: GOOGLE_PLACES_API_KEY
+    googlePlacesApiKey: GOOGLE_PLACES_API_KEY,
+    libraries: ["places"]
   });
 
   const [markers, setMarkers] = React.useState([]);
@@ -47,11 +58,21 @@ export default function Map() {
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map;
-  }, [])
+  }, []);
+
+  const panTo = React.useCallback(({lat, lng}) => {
+  mapRef.current.panTo({lat, lng});
+  mapRef.current.setZoom(14);
+   }, []);
 
   if (loadError) return "Virhe ladatessa karttaa";
   if (!isLoaded) return "Ladataan karttaa...";
-  return <div>    
+
+return <div>    
+
+    <Search panTo={panTo} />
+    <Locate panTo={panTo} />
+
     <GoogleMap 
     mapContainerStyle={mapContainerStyle}
     zoom={8}
@@ -79,4 +100,74 @@ export default function Map() {
        </InfoWindow>): null}
     </GoogleMap>
   </div>;
+}
+
+function Locate({ panTo } ) {
+  return (
+  <button className="locate" onClick={() => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    panTo({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    });
+  },
+  () => null
+  );
+}}>
+
+  <img src="compass.svg" alt="compass - locate me" />
+  </button>
+)}
+
+
+function Search( { panTo }) {
+  const {
+    ready, 
+    value, 
+    suggestions: {status, data}, 
+    setValue, clearSuggestions,}
+     = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 60.169857, lng: () => 24.9383 },
+      radius: 100 * 10,
+    },
+  });
+
+  return (
+  <div className="search">
+    <Combobox 
+     onSelect={async (address) => 
+    {
+      setValue(address, false); 
+      clearSuggestions()
+
+      try {
+        const results =  await getGeocode({ address });
+        const { lat, lng } = await getLatLng(results[0]);
+        panTo({ lat, lng });
+        } 
+        catch(error) {
+        console.log("error")
+      }
+    }}
+    >
+      <ComboboxInput
+       value={value}
+        onChange={(e) => {
+      setValue(e.target.value);
+    }}
+        disabled={!ready}
+        placeholder="Syötä osoite"
+      />
+  <ComboboxPopover>
+    <ComboboxList>
+    {status === "OK" && 
+    
+    data.map(( {id, description }) => (    <ComboboxOption key={id} value={description} />
+    ))}
+    </ComboboxList>
+  </ComboboxPopover>
+  </Combobox>
+  </div>
+  )
 }
