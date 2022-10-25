2b34c4ac1ea4f698f07fc5941f8b4e205864e87b
nikomackk <nikomakin@gmail.com>
2022-10-25 11:29:37 +0300

    Muokattu Homepagea niin, että haetaan vain yhden käyttäjän bongaukset

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { UserAuth } from '../Context/AuthContext';
 import '../Styles/App.css'
-import { getDatabase, ref, push, set, onValue } from "firebase/database";
+import { getDatabase, ref, push, set, onValue, child, get } from "firebase/database";
 import { useEffect, useState } from "react";
 import App from "../App";
 
@@ -13,9 +13,23 @@ const Home = () => {
   const { user } = UserAuth();
   const db = getDatabase();
   const [items, setItems] = useState([]);
-
-
-  useEffect(() => {
+  
+  // HAKEE KÄYTTÄJÄN UID PERUSTEELLA TIEDOT BONGAUKSISTA
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, `users/${user.uid}`)).then((snapshot) => {
+    if(snapshot.exists()) {
+      const data = snapshot.val();
+      const mappedItems = data ? Object.keys(data).map(key => ({key, ...data[key]})) : [];
+      setItems(mappedItems); 
+      
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  
+  /*useEffect(() => {
     const itemsRef = ref(db, 'users/');
     onValue(itemsRef, snapshot => {
       const data = snapshot.val();
@@ -23,7 +37,7 @@ const Home = () => {
       setItems(mappedItems); 
       console.log(mappedItems);   
     });
-  }, []);
+  }, []);*/
 
   return (
     <div>
@@ -33,8 +47,8 @@ const Home = () => {
       </div>
       {items.map((user) => { 
         return <div>
-          <h1>date: {user.spottings.spot.date}</h1>
-          <h1>reknro: {user.spottings.spot.registernumber}</h1>
+          <h1>date: {user.spot.date}</h1>
+          <h1>reknro: {user.spot.registernumber}</h1>
         </div>})}
     </div>
   );
