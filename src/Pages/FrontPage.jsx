2c8020c2d99ea4bdebdab261c56f60091a025f2f
Tuukka Nisonen <tuukkanis@gmail.com>
2022-11-07 12:31:29 +0200

    Merge branch 'master' of https://github.com/Bongaussovellus/Front

--- src/Pages/FrontPage.jsx
+++ src/Pages/FrontPage.jsx
@@ -6,10 +6,7 @@ const FrontPage = () => {
     return (
       <div class="frontpage">
         <h1>Tervetuloa käyttämään Bongaussovellusta!</h1>
-<<<<<<< HEAD
       
-=======
->>>>>>> 553923393edb950d629b08c2eaec1b2666d99efb
       </div>
     )
   }
