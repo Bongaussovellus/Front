1bdc7d0bc26e2188d4b3b8018fa6640fd9a882e6
jvskv <97881987+jvskv@users.noreply.github.com>
2022-11-28 14:27:20 +0200

    API KEYt lähti piiloo
    API KEY:t piilotettu

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,17 @@
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBg6zHBVr-9qLWavt9utuY8HyRLMA05Gs",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "bongausprojekti.firebaseapp.com",
   projectId: "bongausprojekti",
   storageBucket: "bongausprojekti.appspot.com",
   messagingSenderId: "587839363768",
   appId: "1:587839363768:web:037f19cc04440952c4724d",
   measurementId: "G-09S3L75TXC",
-  databaseURL: "https://bongausprojekti-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL:
+    "https://bongausprojekti-default-rtdb.europe-west1.firebasedatabase.app",
 };
-
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,18 +1,26 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
-import 'firebase/compat/auth';
+import "firebase/compat/auth";
 import { initializeApp } from "firebase/app";
-import {collection, addDoc, getFirestore, getDoc, doc, setDoc} from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getFirestore,
+  getDoc,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBg6zHBVr-9qLWavt9utuY8HyRLMA05Gs",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "bongausprojekti.firebaseapp.com",
   projectId: "bongausprojekti",
   storageBucket: "bongausprojekti.appspot.com",
   messagingSenderId: "587839363768",
   appId: "1:587839363768:web:037f19cc04440952c4724d",
   measurementId: "G-09S3L75TXC",
-  databaseURL: "https://bongausprojekti-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL:
+    "https://bongausprojekti-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 // Initialize Firebase
@@ -21,11 +29,10 @@ firebase.initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-
-export const auth = firebase.auth()
+export const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
