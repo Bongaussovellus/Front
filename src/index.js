df2ea3545aba0fc5785fd7cfc57464bb113002aa
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-11-28 23:30:48 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
